1627eb449d802dee98e7886612250063b92f9e3a
Ubuntu <v-tanmgupta@microsoft.com>
2024-03-01 10:45:56 +0000

    checkfreq saving

--- deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
+++ deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
@@ -8,6 +8,29 @@ from deepspeed.utils import logger, log_dist
 from deepspeed.runtime.checkpoint_engine.checkpoint_engine import \
     CheckpointEngine
 
+def _to_cpu(ele, snapshot=None):
+	#while True:
+    if snapshot is None:
+            snapshot = {}
+
+    if hasattr(ele, 'cpu'):
+            snapshot = ele.cpu()
+    elif isinstance(ele, tuple):
+        ele=[0 if x is None else x for x in ele]
+        _to_cpu(ele)
+        # print(type(ele))
+        # # print(ele)
+        # tensortemp=torch.tensor(ele)
+        # snapshot=tensortemp.cpu()
+    elif isinstance(ele, dict):
+        snapshot = {}
+        for k,v in ele.items():
+                snapshot[k] = None
+                snapshot[k] = _to_cpu(v, snapshot[k])
+    elif isinstance(ele, list):
+        snapshot  = [None for _ in range(len(ele))]
+        for idx, v in enumerate(ele):
+                snapshot[idx] = _to_cpu(v, snapshot[idx])
 
 class TorchCheckpointEngine(CheckpointEngine):
 
@@ -19,7 +42,11 @@ class TorchCheckpointEngine(CheckpointEngine):
 
     def save(self, state_dict, path: str):
         logger.info(f"[Torch] Saving {path}...")
-        torch.save(state_dict, path)
+        snapshot={}
+        for name, ref in state_dict.items():
+
+            snapshot[name] = _to_cpu(ref)
+        # torch.save(state_dict, path)
         logger.info(f"[Torch] Saved {path}.")
         return None
 
